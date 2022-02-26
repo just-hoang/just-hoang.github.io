dc31cf0b13091381da5ce378460cb3700a47a778
Hoang Nguyen <baohoang1496@gmail.com>
2022-02-26 15:54:27 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-85683f9623da1a963004.js"
+    "url": "webpack-runtime-799c2090e530753491c7.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "92baf1a34606ee69e28144478ff924c5"
+    "revision": "84e4f5f352712a6b52dc159775b547e2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
