486335a429a786c07f92da7d7da78a053a2200ad
root <ngpbhoang1406@gmail.com>
2022-06-30 15:33:47 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-47a8a5ea9e0f721d565d.js"
+    "url": "webpack-runtime-9ec77a628b1421755572.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9410390218a7062671282deb582f61e4"
+    "revision": "4bb42c2086292471ff20dd89a2975bee"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
